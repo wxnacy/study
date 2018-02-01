e45334e639cc2a5fb3bab01c85da973e63f149ac
wenxiaoning <wenxiaoning@gochinatv.com>
2018-02-01 16:37:36 +0800

    add-pyexecjs

--- python/pyexecjs_demo/app.py
+++ python/pyexecjs_demo/app.py
@@ -10,13 +10,9 @@ import json
 os.environ["EXECJS_RUNTIME"] = "Node"
 os.environ["NODE_PATH"] = os.getcwd()+"/node_modules"
 
-parser = execjs.compile("""
-var uglifyjs = require('uglify-js');
-
-const test = (text) => {
-    return uglifyjs.minify(text);
-}
-                    """)
+f = open(os.getcwd() + "/index.js")
+text = f.read()
+parser = execjs.compile(text)
 
 if __name__ == "__main__":
     obj = parser.call("test", 'function add(first, second) { return first + second;    }')
