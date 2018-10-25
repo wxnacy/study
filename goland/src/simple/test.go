238aae4f38b4350cb3c3d7fe19ac7f406dfa530e
wxnacy <371032668@qq.com>
2018-10-25 15:19:48 +0800

    fix bug

--- goland/src/simple/test.go
+++ goland/src/simple/test.go
@@ -2,10 +2,16 @@ package main
 
 import (
     "fmt"
+    "reflect"
 )
 
 func main() {
     fmt.Println("Hello World") 
-    fmt.Println( 101 / 2 )
+    fmt.Println( float64(101) / 2 )
+    a := float64(101) / 2
+    // fmt.Println( int(float64(33) / 345 * 20))
     fmt.Println( 101 % 2 )
+    fmt.Println(reflect.TypeOf(a))
+
+    fmt.Println(fmt.Sprintf("%.0f", 1.9))
 }
--- python/flask_demo/app/common/email_utils.py
+++ python/flask_demo/app/common/email_utils.py
@@ -11,33 +11,11 @@ from email.mime.multipart import MIMEMultipart
 from email.header import Header
 import requests, json
 
-# 第三方 SMTP 服务
-# mail_host = "smtp.mxhichina.com"  # 设置服务器
-# mail_user = "account@wxnacy.com"  # 用户名
-# mail_pass = "Wxnacy140111"  # 口令
-#
-# sender = 'account@wxnacy.com'
-# receivers = ['371032668@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
-#
-# message = MIMEText('Python 邮件发送测试...', 'plain', 'utf-8')
-# message['From'] = Header("菜鸟教程", 'utf-8')
-# message['To'] = Header("测试", 'utf-8')
-#
-# subject = 'Python SMTP 邮件测试'
-# message['Subject'] = Header(subject, 'utf-8')
-#
-# try:
-#     smtpObj = smtplib.SMTP()
-#     smtpObj.connect(mail_host, 25)  # 25 为 SMTP 端口号
-#     smtpObj.login(mail_user, mail_pass)
-#     smtpObj.sendmail(sender, receivers, message.as_string())
-# except smtplib.SMTPException as e:
-#     print(e)
 
 
 class Email():
-    def __init__(self, host='smtp.mxhichina.com', user='account@wxnacy.com',
-            password='Wxnacy140111', sender='account@wxnacy.com',
+    def __init__(self, host='smtp.mxhichina.com', user='',
+            password='', sender='',
             sender_name='温总博客'):
         try:
             smtpObj = smtplib.SMTP()
