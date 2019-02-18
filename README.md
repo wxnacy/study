edda13dbba1b22b00af5d241aa2ff5557c2ffecf
wxnacy <371032668@qq.com>
2019-02-18 16:55:24 +0800

    add go os user

--- README.md
+++ README.md
@@ -17,3 +17,4 @@
     - [单行进度条](https://github.com/wxnacy/study/blob/master/goland/src/progress/single.go)
     - [带颜色进度条](https://github.com/wxnacy/study/blob/master/goland/src/progress/color.go)
     - [多行进度条](https://github.com/wxnacy/study/blob/master/goland/src/progress/multi.go)
+- [机器用户信息](https://github.com/wxnacy/study/blob/master/goland/src/simple/os_user.go)
---
+++ goland/src/simple/os_user.go
@@ -0,0 +1,43 @@
+package main
+// 使用 os/user 模块获取机器用户信息
+
+import (
+     "fmt"
+     "os/user"
+)
+
+func handleErr(err error){
+    if err != nil {
+        fmt.Println(err)
+    }
+}
+
+func main() {
+    u, err := user.Current()
+    handleErr(err)
+    fmt.Println("当前用户")
+    fmt.Printf("Gid %s\n", u.Gid)
+    fmt.Printf("Uid %s\n", u.Uid)
+    fmt.Printf("Username %s\n", u.Username)
+    fmt.Printf("Name %s\n", u.Name)
+    fmt.Printf("HomeDir %s\n", u.HomeDir)
+
+    u, err = user.Lookup("wxnacy")
+    handleErr(err)
+    fmt.Println("根据 Username wxnacy 查询")
+    fmt.Printf("Gid %s\n", u.Gid)
+    fmt.Printf("Uid %s\n", u.Uid)
+    fmt.Printf("Username %s\n", u.Username)
+    fmt.Printf("Name %s\n", u.Name)
+    fmt.Printf("HomeDir %s\n", u.HomeDir)
+
+    u, err = user.LookupId("501")
+    handleErr(err)
+    fmt.Println("根据 Uid 501 查询")
+    fmt.Printf("Gid %s\n", u.Gid)
+    fmt.Printf("Uid %s\n", u.Uid)
+    fmt.Printf("Username %s\n", u.Username)
+    fmt.Printf("Name %s\n", u.Name)
+    fmt.Printf("HomeDir %s\n", u.HomeDir)
+
+}
