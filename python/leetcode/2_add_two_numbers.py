6f5c9ca0ded901560f1d585105c0ffc30a2f28cc
wxnacy <371032668@qq.com>
2019-03-26 11:27:35 +0800

    update add two numbers

--- python/leetcode/2_add_two_numbers.py
+++ python/leetcode/2_add_two_numbers.py
@@ -17,64 +17,16 @@
 原因：342 + 465 = 807
 '''
 
+import unittest
+import utils
+
 class ListNode(object):
    def __init__(self, x):
        self.val = x
        self.next = None
 
-#  先进行算数，在解析成链表，平均 124 ms ，因为两次循环，所以偏慢
-#  class Solution(object):
-    #  def addTwoNumbers(self, l1, l2):
-        #  """
-        #  :type l1: ListNode
-        #  :type l2: ListNode
-        #  :rtype: ListNode
-        #  """
-        #  n1 = 0
-        #  n2 = 0
-        #  n = 0
-        #  while l1 or l2:
-            #  n1 += l1.val * ( 10 ** n ) if l1 else 0
-            #  n2 += l2.val * ( 10 ** n ) if l2 else 0
-            #  l1 = l1.next if l1 else None
-            #  l2 = l2.next if l2 else None
-            #  n += 1
-        #  n3 = n1 + n2
-        #  l3 = ListNode(0)
-        #  l4 = l3
-        #  while n3 >= 0:
-            #  pre = n3 % 10
-            #  n3 = ( n3 - pre ) // 10
-            #  l4.next = ListNode(pre)
-            #  l4 = l4.next
-            #  if n3 == 0:
-                #  n3 = -1
-        #  return l3.next
-
-# 使用一次循环，再循环的时候就计算，并输出链表，平均 80 ms
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        next_n = 0
-        l3 = ListNode(0)
-        temp = l3
-        while l1 or l2 or next_n:
-            n1 = l1.val if l1 else 0
-            n2 = l2.val if l2 else 0
-            n = n1 + n2 + next_n
-            pre = n % 10
-            next_n = 1 if n >= 10 else 0
-            temp.next = ListNode(pre)
-            temp = temp.next
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-        return l3.next
-
 def makeListNode(arr: list):
+    '''使用数组生成链表'''
     ln = ListNode(0)
     l = ln
     for i in arr:
@@ -83,25 +35,106 @@ def makeListNode(arr: list):
     return ln.next
 
 def listNodeToArray(l: ListNode):
+    '''链表转为数组'''
     arr = []
     while l:
         arr.append(l.val)
         l = l.next
     return arr
 
+def addTwoNumbers1(l1, l2):
+    """
+    先计算链表代表的数字，再相加，并转为链表
+    :type l1: ListNode
+    :type l2: ListNode
+    :rtype: ListNode
+    """
+    n1 = 0
+    n2 = 0
+    n = 0
+    while l1 or l2:
+        n1 += l1.val * ( 10 ** n ) if l1 else 0     # 计算链表代表的数字
+        n2 += l2.val * ( 10 ** n ) if l2 else 0
+        l1 = l1.next if l1 else None
+        l2 = l2.next if l2 else None
+        n += 1
+    n3 = n1 + n2                                    # 数字相加
+    l3 = ListNode(0)
+    l4 = l3
+    while n3 >= 0:
+        pre = n3 % 10                               # 末尾取余并构建链表
+        n3 = ( n3 - pre ) // 10
+        l4.next = ListNode(pre)
+        l4 = l4.next
+        if n3 == 0:
+            n3 = -1
+    return l3.next
+
+def addTwoNumbers2(l1, l2):
+    """
+    再看下题目，发现结果链表的每位，都是传参的两个链表的每位相加得到的
+    由此可以使用一次循环，直接计算每位数字并输出到链表
+    :type l1: ListNode
+    :type l2: ListNode
+    :rtype: ListNode
+    """
+    next_n = 0
+    l3 = ListNode(0)
+    temp = l3
+    while l1 or l2 or next_n:           # l1、l2 循环后，如果有进位，在执行一次
+        n1 = l1.val if l1 else 0
+        n2 = l2.val if l2 else 0
+        n = n1 + n2 + next_n            # 链表数字相加，并加上一个链表的进位
+        pre = n % 10                    # 取余数，并构建该层链表
+        next_n = 1 if n >= 10 else 0    # 判断是否有进位
+        temp.next = ListNode(pre)
+        temp = temp.next
+        l1 = l1.next if l1 else None
+        l2 = l2.next if l2 else None
+    return l3.next
+
+
+class TestMain(unittest.TestCase):
+
+    def setUp(self):
+        '''before each test function'''
+        pass
+
+    def tearDown(self):
+        '''after each test function'''
+        pass
+
+    def test_addTwoNumbers1(self):
+        l3 = addTwoNumbers1(makeListNode([2, 4, 3]), makeListNode([5, 6, 4]))
+        self.assertEqual(listNodeToArray(l3), [7, 0, 8])
+
+        l3 = addTwoNumbers1(makeListNode([0]), makeListNode([0]))
+        self.assertEqual(listNodeToArray(l3), [0])
+
+        l3 = addTwoNumbers1(makeListNode([1, 8]), makeListNode([0]))
+        self.assertEqual(listNodeToArray(l3), [1, 8])
+
+        l3 = addTwoNumbers1(makeListNode([5]), makeListNode([5]))
+        self.assertEqual(listNodeToArray(l3), [0, 1])
+
+    def test_addTwoNumbers2(self):
+        l3 = addTwoNumbers2(makeListNode([2, 4, 3]), makeListNode([5, 6, 4]))
+        self.assertEqual(listNodeToArray(l3), [7, 0, 8])
+
+        l3 = addTwoNumbers2(makeListNode([0]), makeListNode([0]))
+        self.assertEqual(listNodeToArray(l3), [0])
+
+        l3 = addTwoNumbers2(makeListNode([1, 8]), makeListNode([0]))
+        self.assertEqual(listNodeToArray(l3), [1, 8])
+
+        l3 = addTwoNumbers2(makeListNode([5]), makeListNode([5]))
+        self.assertEqual(listNodeToArray(l3), [0, 1])
 
 if __name__ == "__main__":
-    s = Solution()
+    count = 10000
+    utils.print_func_run_time(count, addTwoNumbers1,
+        l1 = makeListNode([2, 4, 3]), l2 = makeListNode([5, 6, 4]))
+    utils.print_func_run_time(count, addTwoNumbers2,
+        l1 = makeListNode([2, 4, 3]), l2 = makeListNode([5, 6, 4]))
+    unittest.main()
 
-    l3 = s.addTwoNumbers(makeListNode([2, 4, 3]), makeListNode([5, 6, 4]))
-    print(listNodeToArray(l3) == [7, 0, 8])
-
-    l3 = s.addTwoNumbers(makeListNode([0]), makeListNode([0]))
-    print(listNodeToArray(l3) == [0])
-
-    l3 = s.addTwoNumbers(makeListNode([1, 8]), makeListNode([0]))
-    print(listNodeToArray(l3) == [1, 8])
-
-    l3 = s.addTwoNumbers(makeListNode([5]), makeListNode([5]))
-    #  print(listNodeToArray(l3))
-    print(listNodeToArray(l3) == [0, 1])
